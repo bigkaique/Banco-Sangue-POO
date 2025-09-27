0cc3dcf0cf5be6c737f0148771c6a8d8b7d4f89c
kaique felipe <kaiquefelipe385@gmail.com>
2025-09-27 10:50:12 -0300

    Add files via upload

---
+++ Banco_Sangue.sql
@@ -0,0 +1,8 @@
+CREATE Table Banco_sangue(
+    id int auto_increment primary key,
+    nome varchar(255),
+    cnpj varchar(20),
+    email varchar(255),
+    telefone varchar(11),
+    endereco varchar(500)
+);
